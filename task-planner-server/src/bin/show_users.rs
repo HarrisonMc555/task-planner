988f89278c03332ed1f1ce9b8fe4c628468f786d
Harrison McCullough <mccullough.harrison@gmail.com>
2021-03-13 15:04:32 -0700

    Create show_users binary
    Create the necessary models and helper functions to query the database
    for all users and print them out.

---
+++ task-planner-server/src/bin/show_users.rs
@@ -0,0 +1,24 @@
+extern crate diesel;
+extern crate task_planner_server;
+
+use self::diesel::prelude::*;
+use self::models::*;
+use self::task_planner_server::*;
+
+fn main() {
+    use task_planner_server::schema::users::dsl::*;
+
+    let connection = establish_connection();
+    let results = users
+        .limit(5)
+        .load::<User>(&connection)
+        .expect("Error loading users");
+
+    println!("Displaying {} users", results.len());
+    for user in results {
+        println!(
+            "Username: '{}', display name: '{}'",
+            user.username, user.display_name
+        );
+    }
+}
