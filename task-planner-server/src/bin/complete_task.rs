7c6aeba839f817689e2a43d8f0f99cfa43b3b4fd
Harrison McCullough <mccullough.harrison@gmail.com>
2021-03-15 23:12:16 -0600

    Create complete_task binary

---
+++ task-planner-server/src/bin/complete_task.rs
@@ -0,0 +1,56 @@
+extern crate diesel;
+extern crate task_planner_server;
+
+use self::task_planner_server::{tasks::*, users::*, *};
+use std::io::stdin;
+
+fn main() -> Result<(), diesel::result::Error> {
+    let connection = establish_connection();
+
+    println!("Enter your username");
+    let mut username_buffer = String::new();
+    let user;
+    loop {
+        username_buffer.clear();
+        stdin().read_line(&mut username_buffer).unwrap();
+        let username = username_buffer.trim_end();
+
+        match user_by_username(&connection, username) {
+            Ok(Some(u)) => {
+                user = u;
+                break;
+            }
+
+            Ok(None) => println!(
+                "No user found with username {}. Please try again.",
+                username
+            ),
+
+            Err(e) => return Err(e),
+        }
+    }
+
+    let tasks = crate::tasks::get_incomplete(&connection, user.id).expect("Error loading tasks");
+    if tasks.is_empty() {
+        println!("No incomplete tasks");
+        return Ok(());
+    }
+
+    println!("Displaying {} tasks", tasks.len());
+    for task in tasks {
+        println!("#{} {}", task.id, task.title);
+    }
+
+    println!("Which task to complete?");
+    let mut task_id_buffer = String::new();
+    stdin().read_line(&mut task_id_buffer).unwrap();
+    let task_id = task_id_buffer.trim_end();
+    let task_id = task_id.parse::<i32>().expect("Invalid task ID");
+
+    match complete_task(&connection, task_id) {
+        Ok(task) => println!("Completed task: {:?}", task),
+        Err(e) => println!("Error completing task: {:?}", e),
+    }
+
+    Ok(())
+}
--- task-planner-server/src/tasks.rs
+++ task-planner-server/src/tasks.rs
@@ -18,3 +18,16 @@ pub fn create_task<'a>(conn: &PgConnection, task: &NewTask) -> Result<Task, dies
         .values(task)
         .get_result(conn)
 }
+
+pub fn complete_task(conn: &PgConnection, id: i32) -> QueryResult<Task> {
+    diesel::update(dsl::tasks.find(id))
+        .set(dsl::complete.eq(true))
+        .get_result::<Task>(conn)
+}
+
+pub fn get_incomplete(conn: &PgConnection, user_id: i32) -> QueryResult<Vec<Task>> {
+    dsl::tasks
+        .filter(dsl::user_id.eq(user_id))
+        .filter(dsl::complete.eq(false))
+        .load::<Task>(conn)
+}
