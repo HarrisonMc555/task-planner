0429becadaca2e4ef2419dc187874a7d9cf5d58f
Harrison McCullough <mccullough.harrison@gmail.com>
2021-03-27 21:19:12 -0600

    Add tasks REST endpoint
    The `/user/<user_id>/tasks` endpoint now returns a plain text response
    with the user's tasks. Each task's title is displayed along with a
    checkbox that is "filled" for completed tasks.
    
    This is a simple example and will likely be replaced by a more
    structured form (e.g. JSON) in the future.

--- task-planner-server/src/bin/server.rs
+++ task-planner-server/src/bin/server.rs
@@ -4,11 +4,34 @@ extern crate diesel;
 extern crate rocket;
 extern crate task_planner_server;
 
+use task_planner_server::{connection, tasks};
+
 pub fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+        .mount("/", routes![index, get_user_tasks])
+        .launch();
 }
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
+}
+
+#[get("/user/<user_id>/tasks")]
+fn get_user_tasks(user_id: i32) -> String {
+    let connection = connection::establish_connection();
+    let tasks = match tasks::all(&connection, user_id) {
+        Ok(tasks) => tasks,
+        Err(e) => return format!("{:?}", e),
+    };
+    if tasks.is_empty() {
+        return "No tasks".to_string();
+    }
+    tasks
+        .into_iter()
+        .map(|task| {
+            let checkbox = if task.complete { "X" } else { " " };
+            format!("[{}] {}\n", checkbox, task.title)
+        })
+        .collect()
 }
